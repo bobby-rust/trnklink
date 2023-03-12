33301e1f7d9a16f909e03c1e7e1f586fd900e004
Ronaldo Suarez <ronaldo3055@gmail.com>
2023-03-11 20:00:21 -0500

    Added icons

--- app/(client)/tools/(preview)/icons.js
+++ app/(client)/tools/(preview)/icons.js
@@ -1,7 +1,13 @@
-import { BsYoutube } from "react-icons/bs";
+import { BsYoutube, BsGoogle, BsInstagram } from "react-icons/bs";
 import { ImYoutube2 } from "react-icons/im";
+import {FaSnapchatGhost} from "react-icons/fa";
+import {SlSocialSpotify, } from "react-icons/sl";
+import {SiSpotify, SlSocialSoundcloud} from "react-icons/si";
+import {TiSocialDribbble, TiSocialLastFm, TiSocialTumbler, TiSocialGithub, TiSocialVimeo} from "react-icons/ti";
 
 export const mediaIcons = {
     youtube1: <BsYoutube />,
-    youtube2: <ImYoutube2 />
+    youtube2: <ImYoutube2 />,
+    spotify1: <SlSocialSpotify />,
+    spotify2: <SiSpotify />
 }
